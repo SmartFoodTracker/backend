c0b97a34c91f396b024cba9c0035994df6b7a4df
Andrew Hoskins <andrew.hoskins16@gmail.com>
2017-01-21 17:51:52 -0700

    fix the port on the test

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ import '../lib/index.js';
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
+    http.get('http://127.0.0.1:8080', res => {
       assert.equal(200, res.statusCode);
       done();
     });
