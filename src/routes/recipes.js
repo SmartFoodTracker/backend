0865ee419628defd29982f83c1585fe03bc275e1
Andrew Hoskins <andrew.hoskins16@gmail.com>
2017-02-19 21:53:22 -0700

    api docs for recipes

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,7 +2,24 @@ import request from 'request';
 
 /**
 * @api {get} /recipes Request Recipes Using Ingredients
-* 
+* @apiGroup Recipes
+*
+* @apiParam (required) {String} ingredients comma seperated list
+* @apiParam (optional) {String} cuisine one of the following: african, chinese, japanese, korean, vietnamese, thai, indian, british, irish, french, italian, mexican, spanish, middle eastern, jewish, american, cajun, southern, greek, german, nordic, eastern european, caribbean, or latin american
+* @apiParam (optional) {String} intolerances allergy words, one of the following: dairy, egg, gluten, peanut, sesame, seafood, shellfish, soy, sulfite, tree nut, and wheat
+* @apiParam (optional) {String} query general recipe search string, ex: hamburger
+* @apiParam (optional) {Number} page next 10 results
+* @apiParam (optional) {String} type one of the following: main course, side dish, dessert, appetizer, salad, bread, breakfast, soup, beverage, sauce, or drink
+*
+* @apiSuccessExample {json} Success-Response: 
+*		[
+*			{
+*				title: 'Zesty Tomato Sauce',
+*				image: 'https://spoonacular.com/recipeImages/zesty-tomato-sauce-268411.jpg',
+*				steps: ['Fill pan with water', ...]
+*			}
+*			...
+*		]
 */
 export function getRecipes(req, res) {
 
@@ -11,28 +28,23 @@ export function getRecipes(req, res) {
 
 	let requestUrl = `${ENDPOINT}?includeIngredients=${req.query.ingredients}&number=${RESULT_SIZE}&ranking=2&addRecipeInformation=true&instructionsRequired=true`;
 
-	// enum: places like american, japanese
 	if (req.query.cuisine) {
 		requestUrl = requestUrl + `&cuisine=${req.query.cuisine}`;
 	}
 
-	// enum: allergy things like gluten, nuts
 	if (req.query.intolerances) {
 		requestUrl = requestUrl + `&intolerances=${req.query.intolerances}`;
 	}
 
-	// string: recipe search query like burger
 	if (req.query.query) {
 		requestUrl = requestUrl + `&query=${req.query.query}`;
 	}
 
-	// number: paginated request offset
 	if (req.query.page) {
 		let offset = req.query.page * RESULT_SIZE;
 		requestUrl = requestUrl + `&offset=${offset}`;
 	}
 
-	// enum: type of food like dessert, salad, soup
 	if (req.query.type) {
 		requestUrl = requestUrl + `&type=${req.query.type}`;
 	}
